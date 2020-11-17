562d49e49cb413fadd03758177432e95da012a9a
Kseniya Antipina <karinpark@mail.ru>
2020-11-17 23:08:30 +0300

    added adaptive

--- js/main.js
+++ js/main.js
@@ -7,29 +7,76 @@ $(document).ready(function () {
         nextArrow: '<button type="button" class="slick-next-custom"></button>',
         responsive: [
             {
-                breakpoint: 1100,
+                breakpoint: 1100.98,
                 settings: {
                     slidesToShow: 2,
                     slidesToScroll: 2,
                 }
             },
             {
-                breakpoint: 991,
+                breakpoint: 991.98,
                 settings: {
                     slidesToShow: 2,
                     slidesToScroll: 2
                 }
             },
             {
-                breakpoint: 767,
+                breakpoint: 767.98,
                 settings: {
                     slidesToShow: 1,
-                    slidesToScroll: 1
+                    slidesToScroll: 1,
+                    arrows: false,
                 }
             }
         ]
     });
 });
+
+/*скрипт бургер меню */
+
+let burger = document.getElementById('dropdownMenu');
+dropdownMenu.onclick = function myFunction() {
+
+    let x = document.getElementById('myTopnav');
+    if (x.className === "header__menu-top") {
+        x.className += " responsive";
+        burger.classList.toggle('burger_active');
+    } else {
+        x.className = "header__menu-top";
+        burger.classList.toggle('burger_active');
+    }
+}
+
+
+if (window.innerWidth < 992) {
+    $(".header__menu-top-wrap li a").click(function () {
+        let x = document.getElementById('myTopnav');
+        if (x.className === "header__menu-top") {
+            x.className += " responsive";
+            burger.classList.toggle('burger_active');
+        } else {
+            x.className = "header__menu-top";
+            burger.classList.toggle('burger_active');
+        }
+    });
+}
+
+
+
+
+
+
+/*скрипт скролла шапки*/
+
+$(".header__menu-top-wrap li a").click(function () {
+    var anchor = $(this).attr('href');
+    $('html, body').stop().animate({ scrollTop: $(anchor).offset().top - 164 }, 700);
+    e.preventDefault();
+    return false;
+});
+
+
+/*кастомный select*/
 
 $('.select').on('click', '.placeholder', function () {
     var parent = $(this).closest('.select');
@@ -44,15 +91,51 @@ $('.select').on('click', '.placeholder', function () {
     parent.removeClass('is-open').find('.placeholder').text($(this).text());
 });
 
-window.addEventListener("resize", function () {
-    if (window.innerWidth <= 768) {
-        $('.your-slider').slick('unslick');
-        sliderIsLive = false;
-    }
-    else {
-        if (sliderIsLive) {
-            $('.your-slider').slick();
-            sliderIsLive = true;
-        }
-    }
+
+/*скрипт для слайдера*/
+
+$(document).ready(function () {
+    $('.products-slider').slick({
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: '<button type="button" class="slick-prev-custom" id="product-prev"></button>',
+        nextArrow: '<button type="button" class="slick-next-custom" id="product-next"></button>',
+        responsive: [
+            {
+                breakpoint: 10000,
+                settings: 'unslick'
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+
+                }
+            }
+        ]
+    });
+});
+
+$(document).ready(function () {
+    $('.stages-slider').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 10000,
+                settings: 'unslick'
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
 });
